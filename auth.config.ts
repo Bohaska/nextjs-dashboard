9d8266715c43ea2df7bd52ff8583551a9affa70f
Bohaska <73286691+Bohaska@users.noreply.github.com>
2024-12-08 22:56:51 +0800

    commit auth.config.ts

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnAdmin = nextUrl.pathname.startsWith('/admin');
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnAdmin) {
+                return isLoggedIn && auth?.user?.email === 'admin@admin.com';
+            }
+            if (isOnDashboard) {
+                return isLoggedIn;
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
